fe2c5a50137df8cec578d77eb7bb85bd5156b2e8
kbseah <kb.seah@gmail.com>
2024-12-26 13:06:41 +0800

    Script to convert dictionary file from TSV to JSON

---
+++ tsv2json.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// Convert TSV formatted dictionary table to JSON format
+
+const fs = require("fs");
+const readline = require("readline");
+
+// main ----------------------------------------------------------------------
+
+// read in dictionary data
+let chardict = {};
+const dicttsv = fs.readFileSync("dieziu_gdpi.dict.tsv", "utf-8");
+dicttsv.split(/\r?\n/).forEach(line => {
+  try {
+    let [han, pengim] = line.split(/\t/);
+    if ( han in chardict ) {
+      chardict[han].push(pengim);
+    } else {
+      chardict[han] = [pengim];
+    }
+  } catch(err) {
+    console.error(err);
+  }
+});
+
+fs.writeFile('dieziu_gdpi.dict.json', JSON.stringify(chardict), (err) => {
+  console.log(err);
+});
